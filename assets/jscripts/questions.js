a8187be44b22f4bfd54a2b5720b6fe95dee376c1
Chance Supcoe <chancesupcoe@gmail.com>
2022-01-24 21:22:29 -0500

    complete rework

---
+++ assets/jscripts/questions.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+      title: "Inside which HTML element do we put the JavaScript",
+      multiChoice: ["<script>", "<js>", "<scripting", "<javascript>"],
+      answer: "<script>"
+    },
+  
+    {
+      title: "Where is the correct place to insert a JavaScript?",
+      multiChoice: ["The <body> section", "The <head> section", "Both the <head> section and <body> section are correct", "Neither the <head> section or <body> section are correct"],
+      answer: "Both the <head> section and <body> section are correct"
+    },
+  
+    {
+      title: "How do you create a funtion in JavaScript?",
+      multiChoice: ["function = myFunction()", "funtion myFunction()", "funtion:myFunction()", "(function=)"],
+      answer: "funtion myFunction()"
+    },
+  
+    {
+      title: "How would you write an IF statement in JavaScript?",
+      multiChoice: ["if i==5 then", "if i=5", "if (i==5)", "if i==5 then"],
+      answer: "if (i==5)"
+    },
+  
+    {
+      title: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      multiChoice: ["if i<>5", "if (i<>5)", "if i=!5 then", "if (i!=5)"],
+      answer: "if (i!=5)"
+    },
+
+    {
+      title: "How does a FOR loop start?",
+      multiChoice: ["if i<>5", "if (i<>5)", "if i=!5 then", "if (i!=5)"],
+      answer: "if (i!=5)"
+    },
+
+    {
+      title: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      multiChoice: ["for i=1 to 5", "for(i=0;i<=5;i++)", "for(i<=5;i++)", "for(i=0;i<-5)"],
+      answer: "for(i=0;i<=5;i++)"
+    },
+
+    {
+      title: "How can you detect the client's browser name?",
+      multiChoice: ["browser.name", "client.navName", "navigator.appName", "It is not possible"],
+      answer: "navigator.appName"
+    },
+
+    {
+      title: "Which event occurs when the user clicks on an HTML element?",
+      multiChoice: ["onclick", "onmouseover", "onmouseclick", "onchange"],
+      answer: "onclick"
+    },
+
+    {
+      title: "Which operator is used to assign a value to a variable?",
+      multiChoice: ["=", "*", "-", "X"],
+      answer: "="
+    }
+  ];
